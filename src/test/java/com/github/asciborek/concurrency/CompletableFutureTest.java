43b44f259da1b3a8ce19c6f1bf87b6cadcb5875c
Aleksander Ściborek <aleksandersciborek@gmail.com>
2025-02-01 00:45:32 +0100

    Use VirtualThreadPerTaskExecutor as CompletableFuture ExecutorService

--- src/test/java/com/github/asciborek/concurrency/CompletableFutureTest.java
+++ src/test/java/com/github/asciborek/concurrency/CompletableFutureTest.java
@@ -16,22 +16,22 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 @TestInstance(Lifecycle.PER_CLASS)
 public class CompletableFutureTest {
 
-  private final ExecutorService executor = Executors.newCachedThreadPool();
+  private final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
   @Test
   void anyOfReturnsFirstCompletedFuture() {
     CompletableFuture<String> firstCompletedFuture = CompletableFuture.supplyAsync(() -> {
       sleep(2);
       return "firstCompletedFuture";
-    });
+    }, executor);
     CompletableFuture<Integer> secondCompletedFuture = CompletableFuture.supplyAsync(() -> {
       sleep(6);
       return -1;
-    });
+    }, executor);
     CompletableFuture<Long> thirdCompletedFuture = CompletableFuture.supplyAsync(() -> {
       sleep(8);
       return 1L;
-    });
+    }, executor);
     CompletableFuture<Object> anyOfFuture = CompletableFuture.anyOf(secondCompletedFuture, thirdCompletedFuture, firstCompletedFuture);
     Awaitility.await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> Assertions.assertThat(anyOfFuture.get()).isEqualTo("firstCompletedFuture"));
   }
@@ -41,11 +41,11 @@ public class CompletableFutureTest {
     CompletableFuture<String> exceptionallyComplementedFuture = CompletableFuture.supplyAsync(() -> {
       sleep(1);
       throw new RuntimeException();
-    });
+    }, executor);
     CompletableFuture<String> complementedFuture = CompletableFuture.supplyAsync(() -> {
       sleep(3);
       return "completedFuture";
-    });
+    }, executor);
     CompletableFuture<Object> anyOfFuture = CompletableFuture.anyOf(complementedFuture, exceptionallyComplementedFuture);
 
     Awaitility.await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> Assertions.assertThatThrownBy(anyOfFuture::get).isInstanceOf(
@@ -58,7 +58,7 @@ public class CompletableFutureTest {
     CompletableFuture.<String>supplyAsync(() -> {
           sleep(2);
           return null;
-        })
+        }, executor)
         .thenApply(String::length)
         .thenAccept(_ -> thenAcceptWasCalled.set(true));
     Awaitility.await()
@@ -71,7 +71,7 @@ public class CompletableFutureTest {
     CompletableFuture<Integer> future = CompletableFuture.<String>supplyAsync(() -> {
           sleep(5);
           return null;
-        })
+        }, executor)
         .thenApply(String::length)
         .thenApply(length -> {
           sleep(10);
